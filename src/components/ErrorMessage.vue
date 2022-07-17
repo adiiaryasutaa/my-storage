bbabb53b44ea9b6afc528a665b40867a32a18d76
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2022-07-17 18:50:46 +0800

    added Error Message component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <p v-show="error" class="text-red-600 text-xs font-lato font-bold">{{ error }}</p>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "ErrorMessage",
+  props: {
+    error: String,
+  },
+  setup(props) {
+    const error = ref(props.error);
+
+    return { error };
+  },
+};
+</script>
