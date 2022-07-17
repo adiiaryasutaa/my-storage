667d9c7f937d04a17e106163e8a22a7f40084d3b
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2022-07-17 18:53:18 +0800

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-const routes = []
+const routes = [
+	{
+		path: '/login',
+		name: 'login',
+		meta: {
+			title: 'Login',
+		},
+		component: () => import('../views/LoginView.vue'),
+	},
+	{
+		path: '/',
+		name: 'home',
+		meta: {
+			title: 'My Storage',
+		},
+		component: () => import('../views/HomeView.vue'),
+	},
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+	document.title = to.meta.title;
+	next();
+});
+
+export default router;
