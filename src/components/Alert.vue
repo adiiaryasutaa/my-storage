bffa7f11dfde67563e852f1c130e673edd14a56a
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2022-07-17 18:48:28 +0800

    added Alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+		class="border border rounded font-lato font-semibold px-4 py-2"
+		:class="{
+  		'bg-green-200 border-green-400 text-green-600': type === 'success',
+  		'bg-red-200 border-red-400 text-red-600': type === 'error',
+  		'bg-orange-200 border-orange-400 text-orange-600': type === 'warning',
+    }"
+  >
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props: {
+    message: String,
+    type: String,
+  }
+}
+</script>
