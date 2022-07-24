19cc3d3fccdc15793efc6d9498ca396eb8f5e4ee
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2022-07-24 20:58:48 +0800

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+	name: 'Footer',
+};
+</script>
+
+<template>
+	<footer>
+		<div class="text-sm text-slate-500 font-lato sm:text-center">
+			<span>© 2022 <a href="https://adiaryasuta.vercel.app" class="hover:text-indigo-700 active:text-indigo-800">Adi Aryasuta</a>. All Rights Reserved.</span>
+		</div>
+	</footer>
+</template>
+
+<style scoped>
+footer {
+	@apply border-t border-slate-200 p-4;
+}
+</style>
