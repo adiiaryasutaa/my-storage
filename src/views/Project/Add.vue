1219f93f7cadd3c7aee3d2c0c3c5a2154433cffa
adiiaryasutaa <adiaryasuta.dev@gmail.com>
2022-07-26 23:33:44 +0800

    add add icon

---
+++ src/views/Project/Add.vue
@@ -0,0 +1,19 @@
+<script>
+import ProjectThumbnailInput from '@/components/Project/ProjectThumbnailInput';
+
+export default {
+	name: 'Add',
+	components: { ProjectThumbnailInput },
+};
+</script>
+
+<template>
+	<section class="container mx-auto p-4">
+		<div class="flex flex-col items-center space-y-4">
+			<h1 class="text-slate-900 font-lato font-bold">Add Project</h1>
+			<div class="w-full md:w-96">
+				<ProjectThumbnailInput />
+			</div>
+		</div>
+	</section>
+</template>
